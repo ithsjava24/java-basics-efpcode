96e36b106b2303a707c28bb2b48148112d08de20
efpcode <efpjobhunt@gmail.com>
2024-09-14 18:36:26 +0200

    Started with a naive approach. Added displayMenu, datainput, while-loop.
    Status: nothing really passes.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,8 +1,47 @@
 package org.example;
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
+    public static void displayMenu() {
+        System.out.printf("""
+                "Elpriser
+                ========
+                1. Inmatning
+                2. Min, Max och Medel
+                3. Sortera
+                4. Bästa Laddningstid (4h)
+                e. Avsluta
+                "
+                """);
+    }
+
+    public ArrayList<String> dataInput(Scanner data){
+        String values = data.nextLine();
+        ArrayList<String> data = values.split("\n");
+        return data;
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello There!");
+        Scanner scanner = new Scanner(System.in);
+        displayMenu();
+        while(true){
+           String input = scanner.nextLine().toLowerCase();
+           if(input.equals("e")){
+               break;
+           }
+           else if(input.equals("1")){
+              ArrayList<String> data = dataInput(scanner);
+               System.out.printf("%s",data);
+           }
+           displayMenu();
+
+
+        }
+
+        }
+
     }
-}
+
